6673676a07b7b31cbf4c884d1e345808f8df54cb
Ahmad-Kun <a.farizi48@gmail.com>
2023-03-05 21:28:23 +0700

    Insert the Data to Spreadsheet

--- Arduino_Projek_1/Arduino_Projek_1.ino
+++ Arduino_Projek_1/Arduino_Projek_1.ino
@@ -1,6 +1,8 @@
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
+#include "TRIGGER_WIFI.h"               
+#include "TRIGGER_GOOGLESHEETS.h"  
 // #include <SMTP.h>
 
 //Define DHT11
@@ -11,6 +13,11 @@
 #define LED_pin1 D5
 #define LED_pin2 D8
 
+//
+char column_name_in_sheets[ ][20] = {"value1","value2"};                        /*1. The Total no of column depends on how many value you have created in Script of Sheets;2. It has to be in order as per the rows decided in google sheets*/
+String Sheets_GAS_ID = "AKfycbwrllNXDVMYWmvTfuXjh16ivcNJ-xLVkT0JMvZZ3BSvcmoWwFWRkw6_GLQ9Te0Mp78Q4A";                                         /*This is the Sheets GAS ID, you need to look for your sheets id*/
+int No_of_Parameters = 2;      
+
 DHT dht(DHT11PIN, DHTTYPE, 15);
 LiquidCrystal_I2C lcd(0x27, 20, 2); // CHANGE THE 0X27 ADDRESS TO YOUR SCREEN ADDRESS IF NEEDED
 
@@ -18,7 +25,7 @@ String apiKey = "KFZ34WQXEQN8BMYL";
 const char* ssid = "SysAdmin";
 const char* password = "Sys@dm1n123";
 const char* server = "api.thingspeak.com";
-WiFiClient client;
+// WiFiClient client;
 
 void setup() {
   //Setup LED, Serial, DHT, LCD
@@ -38,7 +45,8 @@ void setup() {
     delay(500);
     digitalWrite(LED_pin1, LOW);
   }
-
+  WIFI_Connect(ssid, password);
+  Google_Sheets_Init(column_name_in_sheets, Sheets_GAS_ID, No_of_Parameters );
   //LCD, Lampu D6 Hidup Jika Koneksi Wifi Berhasil
   lcd.backlight();
   digitalWrite(LED_pin2, HIGH);
@@ -63,5 +71,6 @@ void loop() {
   Serial.print(" Humidity ");
   Serial.print(lembab);
   Serial.println("%");
+  Data_to_Sheets(No_of_Parameters,  suhu,  lembab);
   delay(2000);
 }
