1a864b4b336b4e60c6cd48542d981a5ad120ebcf
Ahmad-Kun <a.farizi48@gmail.com>
2023-03-09 08:13:47 +0700

    Final Draft v.1

--- suhu/app/Config/Routes.php
+++ suhu/app/Config/Routes.php
@@ -30,7 +30,6 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->match(['get', 'post'], 'email', 'Home::index');
 
 /*
  * --------------------------------------------------------------------
--- suhu/app/Controllers/Home.php
+++ suhu/app/Controllers/Home.php
@@ -3,7 +3,6 @@
 namespace App\Controllers;
 use CodeIgniter\Controller;
 use App\Models\indexModel;
-use App\Models\userModel;
 
 class Home extends BaseController
 {
@@ -12,54 +11,26 @@ class Home extends BaseController
     function __construct(){
         $this->ews = new indexModel();
         $this->email = \Config\Services::email();
+        $this->email = service('email');
+        $this->email->setTo('emailtesarduino@gmail.com');
+        $this->email->setFrom('www.petrochina.co.id', 'Early Warning System');
     }
 
     public function index()
     {   
-        $email = service('email');
-        $email->setTo('emailtesarduino@gmail.com');
-        $email->setFrom('www.petrochina.co.id', 'Early Warning System');
+        $query = $this->ews->where('status', false)->first();
+        
+        if ($query['temp'] >= 24) {
+            $email->setSubject('High Temperature Alert');
+            $email->setMessage('Temperature is ' + $query['temp'] + ' degrees. Please take necessary action.');
 
-        $email->setSubject($subject);
-        $email->setMessage($message);
-
-        if (!$email->send()) {
-            $data = $email->printDebugger(['headers']);
-            print_r($data);
-        }
-        $data['ews'] = $this->ews->findAll();
-        return view('index', $data);
-    }
-
-    public function checkTemp()
-    {
-        // Get all temperature records that have not been checked
-        $model = new indexModel();
-        $data = $model->where('status', false)->findAll();
-
-        // Loop through each record and check if temperature is above 24 degrees
-        foreach ($data as $record) {
-            if ($record['temp'] > 24) {
-                // Send email notification to all users
-                $userModel = new userModel();
-                $users = $userModel->findAll();
-                
-                foreach ($users as $user) {
-                    $tujuan = "tanzjder@gmail.com";
-                    $this->email->setTo($tujuan);
-                    $sender = "emailtesarduino@gmail.com";
-                    $this->email->setFrom($sender);
-                    $subject = "High Temperature Alert";
-                    $this->email->setSubject($subject);
-                    $message = "Temperature is above 24 degrees. Please take necessary action.";
-                    $this->email->setMessage($message);
-                    // $this->email->setNewline(“\r\n”);
-                    $this->email->send();
-                }
-                
-                // Update record status to checked
-                $model->update($record['id'], ['status' => true]);
+            if (!$email->send()) {
+                $data = $email->printDebugger(['headers']);
+                print_r($data);
             }
         }
+        $this->ews->update($query['id'], ['status' => true]);
+        $data['ews'] = $this->ews->findAll();
+        return view('index', $data);
     }
 }
