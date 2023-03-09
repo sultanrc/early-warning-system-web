51dbdf3e4105cdab247a944834aafca50a9e6c39
Ahmad Al Farizi <74691228+Ahmad-Kun@users.noreply.github.com>
2023-03-09 15:06:51 +0700

    Update Arduino_Projek_1.ino

--- Arduino_Projek_1/Arduino_Projek_1.ino
+++ Arduino_Projek_1/Arduino_Projek_1.ino
@@ -3,17 +3,12 @@
 #include <LiquidCrystal_I2C.h>
 #include "TRIGGER_WIFI.h"               
 #include "TRIGGER_GOOGLESHEETS.h"  
-// #include <SMTP.h>
 
-//Define DHT11
 #define DHT11PIN D7
 #define DHTTYPE DHT11
-
-//
 #define LED_pin1 D5
 #define LED_pin2 D8
 
-//
 char column_name_in_sheets[ ][20] = {"value1","value2"};                        /*1. The Total no of column depends on how many value you have created in Script of Sheets;2. It has to be in order as per the rows decided in google sheets*/
 String Sheets_GAS_ID = "AKfycbwrllNXDVMYWmvTfuXjh16ivcNJ-xLVkT0JMvZZ3BSvcmoWwFWRkw6_GLQ9Te0Mp78Q4A";                                         /*This is the Sheets GAS ID, you need to look for your sheets id*/
 int No_of_Parameters = 2;      
@@ -24,8 +19,6 @@ LiquidCrystal_I2C lcd(0x27, 20, 2); // CHANGE THE 0X27 ADDRESS TO YOUR SCREEN AD
 String apiKey = "KFZ34WQXEQN8BMYL";
 const char* ssid = "SysAdmin";
 const char* password = "Sys@dm1n123";
-const char* server = "api.thingspeak.com";
-// WiFiClient client;
 
 void setup() {
   //Setup LED, Serial, DHT, LCD
