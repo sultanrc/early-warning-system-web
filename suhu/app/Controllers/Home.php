be1a6fef04a73c0e5efbbf433ec7c74cf34e5a74
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2023-04-07 10:39:47 +0700

    Update controller

--- suhu/app/Controllers/Home.php
+++ suhu/app/Controllers/Home.php
@@ -15,60 +15,38 @@ class Home extends BaseController
     function __construct()
     {
         $this->ews = new indexModel();
-        $this->email = \Config\Services::email();
-        $this->email = service('email');
-        $this->email->setTo('emailtesarduino@gmail.com');
-        $this->email->setFrom('www.petrochina.co.id', 'Early Warning System');
         helper('url', 'form'); // load URL helper
-
     }
 
     public function index()
-    {
-        // exec('python Python/ews.py > /dev/null &');
-        
-        $query = $this->ews->where('status', false)->first();
-        
-        if ($query) {
-            if ($query['temp'] >= 24) {
-                $this->email->setSubject('High Temperature Alert');
-                $message = 'Temperature is ' . strval($query['temp']) . ' degrees. Please take necessary action.';
-                $this->email->setMessage($message);
-                if (!$this->email->send()) {
-                    $data = $this->email->printDebugger(['headers']);
-                    print_r($data);
-                }
-            }
-            $this->ews->update($query['id'], ['status' => true]);
-        }
-        
+    {   
         $data['ews'] = $this->ews->findAll();
         helper('form');
         return view('index', $data);
     }
-    
-     public function insert($temp, $hum, $fa)
-    {
-        // Get the current date and time
-        $date = date('Y-m-d');
-        $time = date('H:i:s');
 
-        // Set empty variable for status
-        $status = '';
+    // public function insert($temp, $hum, $fa)
+    // {
+    //     // Get the current date and time
+    //     $date = date('Y-m-d');
+    //     $time = date('H:i:s');
+
+    //     // Set empty variable for status
+    //     $status = '';
         
-        $data = array(
-            'date' => $date,
-            'time' => $time,
-            'temp' => $temp,
-            'hum' => $hum,
-            'fa' => $fa,
-            'status' => $status
+    //     $data = array(
+    //         'date' => $date,
+    //         'time' => $time,
+    //         'temp' => $temp,
+    //         'hum' => $hum,
+    //         'fa' => $fa,
+    //         'status' => $status
     
-        );
-        $this->ews->insert($data); 
+    //     );
+    //     $this->ews->insert($data); 
 
-        return redirect()->to('/');
-    }
+    //     return redirect()->to('/');
+    // }
 
     public function generate()
     {
